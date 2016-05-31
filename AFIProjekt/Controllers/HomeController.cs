0e0c0bab77f48c8b72a5660b7cd7773db4c4507a
Mattias Johansson <mattiasjohansson001@gmail.com>
2016-05-31 13:36:16 +0200

    model2

--- AFIProjekt/Controllers/HomeController.cs
+++ AFIProjekt/Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace AFIProjekt.Controllers
 
        public ActionResult LoveCalculator()
         {
+
+            AFIProjektEntities1 db = new AFIProjektEntities1();
             Random random = new Random();
             int love = random.Next(1, 100);
             int movieid = random.Next(0, 10);
